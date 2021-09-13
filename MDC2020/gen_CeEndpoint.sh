554791b5473e3168bf9012e0016cf4c6856ee98f
sophieMu2e <sophie@fnal.gov>
2021-09-13 13:06:12 -0500

    storing

--- MDC2020/gen_CeEndpoint.sh
+++ MDC2020/gen_CeEndpoint.sh
@@ -11,7 +11,7 @@ rm template.fcl
 echo '#include "Production/JobConfig/primary/CeEndpoint.fcl"' >> template.fcl
 echo physics.filters.TargetStopResampler.mu2e.MaxEventsToSkip: ${nskip} >> template.fcl
 #
-generate_fcl --dsconf=MDC2020$1 --dsowner=brownd --run-number=1210 --description=CeEndpoint --events-per-job=4000 --njobs=100 \
+generate_fcl --dsconf=MDC2020$1 --dsowner=sophie --run-number=1210 --description=CeEndpoint --events-per-job=1000 --njobs=100 \
 --embed template.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusTargetStopsCat.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
