8e1fef53e87faa86d5962840a556d914f9e02b3f
sophieMu2e <sophie@fnal.gov>
2023-02-09 18:56:47 -0600

    new script works

--- Scripts/generate_Primary.sh
+++ Scripts/generate_Primary.sh
@@ -3,7 +3,9 @@
 # create fcl for producing primaries from stopped particles
 # this script requires mu2etools and dhtools be setup
 #
-#  source gen_Primary.sh CeEndpoint MDC2020 p v Muminus 1000 4000 -11 0 110 Offline/Mu2eG4/geom/bfgeom_reco_altDS11_helical_v01.txt
+# Usage: bash generate_Primary_v2.sh --primary CeEndpoint --pcamp MDC2020v --scamp MDC2020p --type Muminus --njobs 1000 --events 4000 --pdg 11 --start 0 --end 110 --field Offline/Mu2eG4/geom/bfgeom_reco_altDS11_helical_v01.txt
+#
+# Note: User can omit flat (pdg, startmom and enedmom) arguments without issue. Field argument also generally will not be used
 
 # The main input parameters needed for any campaign
 PRIMARY="" # is the primary
@@ -14,74 +16,64 @@ JOBS="" # is the number of jobs
 EVENTS="" # is the number of events/job
 
 # The following can be overridden if needed
-PDG=11 #is the pdgId of the particle to generate
+PDG=11 #is the pdgId of the particle to generate (for flat only)
 FIELD="Offline/Mu2eG4/geom/bfgeom_no_tsu_ps_v01.txt" #optional (for changing field map)
-STARTMOM=0 # optional (for flat)
-ENDMOM=110 # optional (for flat)
+STARTMOM=0 # optional (for flat only)
+ENDMOM=110 # optional (for flat only)
 OWNER=mu2e
 RUN=1202
 DESC=${PRIMARY} # can override if more detailed tag is needed
 
-# Function: Print a help message.
-usage() {
-  echo "Usage: $0 [ -p PRIMARY ] [ -v PRIMARY_CAMPAIGN ] [ -s STOPS_CAMPAIGN ] [-t TYPE][ -j JOBS ][ -e EVENTS ][ -pdg PDG ] [-f FIELD] [-smom STARTMOM] [-emom ENDMOM] [-o OWNER] [-r RUN] [ -d DESC ]" 1>&2 
-}
-
 # Function: Exit with error.
 exit_abnormal() {
-  usage
   exit 1
 }
 
 # Loop: Get the next option;
-while getopts ":p:v:s:t:j:e:r:i:f:a:z:o:r:d:-:" options; do
-  case "${options}" in                    
-    p)                                    # If the option is p,
-      PRIMARY=${OPTARG}                  # set $PRIMARY to specified value.
-      ;;
-    v)                                    
-      PRIMARY_CAMPAIGN=${OPTARG}                   
-      ;;
-    s)                                    
-      STOPS_CAMPAIGN=${OPTARG}                     
-      ;;
-    t)                                    
-      TYPE=${OPTARG}                       
-      ;;
-    j)                                    
-      JOBS=${OPTARG}                   
-      ;;
-    e)                                    
-      EVENTS=${OPTARG}                      
-      ;;
-    i)                                   
-      PDG=${OPTARG}                      
-      ;;
-    f)                                   
-      FIELD=${OPTARG}                      
-      ;;
-    a)                                   
-      STARTMOM=${OPTARG}                      
-      ;;
-    z)                                   
-      ENDMOM=${OPTARG}                      
-      ;;
-    o)                                   
-      OWNER=${OPTARG}                      
-      ;;
-    r)                                   
-      RUN=${OPTARG}                      
-      ;;
-    d)                                   
-      DESC=${OPTARG}                      
-      ;;
+while getopts ":-:" options; do
+  case "${options}" in      
     -)                                   
-      case "${OPTARG}" in
-                boo)
-                    
-                  echo "test " >&2;
-                ;;                    
-      esac;;
+      case "${OPTARG}" in               
+        primary)                                  
+          PRIMARY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))      
+          ;;
+        pcamp)                                   
+          PRIMARY_CAMPAIGN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                
+          ;;
+        scamp)                                    
+          STOPS_CAMPAIGN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                  
+          ;;
+        type)                         
+          TYPE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                 
+          ;;
+        njobs)                                    
+          JOBS=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                 
+          ;;
+        events)                                    
+          EVENTS=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                      
+          ;;
+        pdg)                                   
+          PDG=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                   
+          ;;
+        field)                                   
+          FIELD=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                    
+          ;;
+        start)                                   
+          STARTMOM=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                   
+          ;;
+        end)                                   
+          ENDMOM=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                  
+          ;;
+        owner)                                   
+          OWNER=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
+          ;;
+        run)                                   
+          RUN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                  
+          ;;
+        desc)                                   
+          DESC=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                 
+          ;;  
+        esac;;             
     :)                                    # If expected argument omitted:
       echo "Error: -${OPTARG} requires an argument."
       exit_abnormal                       # Exit abnormally.
@@ -89,7 +81,7 @@ while getopts ":p:v:s:t:j:e:r:i:f:a:z:o:r:d:-:" options; do
     *)                                    # If unknown (any other) option:
       exit_abnormal                       # Exit abnormally.
       ;;
-  esac
+    esac
 done
 
 # Test: run a test to check the SimJob for this campaign verion exists TODO 
@@ -115,7 +107,7 @@ else
   resampler=${TYPE}StopResampler
 fi
 
-echo "getting sam locations"
+
 samweb list-file-locations --schema=root --defname="$dataset"  | cut -f1 > Stops.txt
 # calucate the max skip from the dataset
 nfiles=`samCountFiles.sh $dataset`
@@ -123,7 +115,7 @@ nevts=`samCountEvents.sh $dataset`
 let nskip=nevts/nfiles
 # write the template
 rm -f primary.fcl
-echo "adding primary fcl"
+
 if [[ "${TYPE}" == "Cosmic" ]]; then
   echo "#include \"Production/JobConfig/cosmic/S2Resampler${PRIMARY}.fcl\"" >> primary.fcl
 else
@@ -140,7 +132,7 @@ fi
 #
 # now generate the fcl
 #
-echo "generating"
+
 generate_fcl --dsconf=${PRIMARY_CAMPAIGN} --dsowner=${OWNER} --run-number=${RUN} --description=${PRIMARY} --events-per-job=${EVENTS} --njobs=${JOBS} \
   --embed primary.fcl --auxinput=1:physics.filters.${resampler}.fileNames:Stops.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
