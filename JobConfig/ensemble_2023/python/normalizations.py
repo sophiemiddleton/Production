7ade56ea4a1ad56f9ac40f9c3fb0448625d903ae
sophieMu2e <sophie@fnal.gov>
2023-11-20 10:13:16 -0600

    normalizations

--- JobConfig/ensemble_2023/python/normalizations.py
+++ JobConfig/ensemble_2023/python/normalizations.py
@@ -2,11 +2,17 @@ import ROOT
 import math
 import os
 
-mean_PBI = 3.9e7 # in JobConfig/mixing/prolog.fcl  protonBunchIntensity.extendedMean
-dutyfactor = 0.25 # 43.1ms+5ms on spill x8, then 1020ms off spill
+# TODO - which of these numbers are needed?
+mean_PBI_low = 1.6e7
+mean_PBI_high = 3.9e7
+pulses_per_second = 1/(1695e-9)
+mean_PBI = mean_PBI_low*0.75 + mean_PBI_high*0.25
+dutyfactor = 0.323 # from SU2020
 ub_per_year = 365*24*60*60./1695e-9*dutyfactor
+#POT_low = dutyfactor*pulses_per_second*mean_PBI_low*livetime
+#POT_high = dutyfactor*pulses_per_second*mean_PBI_high*livetime
 POT_per_year = ub_per_year*mean_PBI
-stopped_mu_per_POT = 0.00187
+stopped_mu_per_POT = 0.00155
 
 def cry_normalization(livetime):
   cry_expected_rate = 253440 #Hz
@@ -16,7 +22,20 @@ def cry_normalization(livetime):
   cry_expected_per_year = ub_per_year * cry_expected_per_ub
   return cry_expected_per_year * livetime
 
+def corsika_normalization(livetime): ### TODO: we need to get this rate!!!!!
+  cry_expected_rate = 253440 #Hz
+  cry_tmin = 450e-9
+  cry_tmax = 1705e-9
+  cry_expected_per_ub = cry_expected_rate*(cry_tmax-cry_tmin)
+  cry_expected_per_year = ub_per_year * cry_expected_per_ub
+  return cry_expected_per_year * livetime
 
+def ipaMichel_normalization(livetime):
+  IPA_stopped_mu_per_POT = 8e-8
+  IPA_decays_per_stopped_muon = 0.92 # carbon
+
+  return POT_per_year * IPA_stopped_mu_per_POT * IPA_decays_per_stopped_muon * livetime
+  
 def ce_normalization(livetime, rue):
   captures_per_stopped_muon = 0.609
   return POT_per_year * stopped_mu_per_POT * captures_per_stopped_muon * livetime * rue
