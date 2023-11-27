50b388489fa0fc877d01a4084121204499791a26
sophieMu2e <sophie@fnal.gov>
2023-11-27 09:29:35 -0600

    DIO works, path needs fixing eventually

--- JobConfig/ensemble_2023/python/getNorms.py
+++ JobConfig/ensemble_2023/python/getNorms.py
@@ -21,6 +21,8 @@ dbtool.run()
 rr = dbtool.getResult()
 #print(rr)
 lines= rr.split("\n")
+
+# get number of target muon stops:
 target_stopped_mu_per_POT = 1.0
 rate = 1.0
 for line in lines:
@@ -31,6 +33,7 @@ for line in lines:
         target_stopped_mu_per_POT = rate * 1000 
 print(f"Final stops rate {target_stopped_mu_per_POT}")
 
+# get number of ipa muon stops:
 ipa_stopped_mu_per_POT = 1.0
 rate = 1.0
 for line in lines:
@@ -41,14 +44,44 @@ for line in lines:
         ipa_stopped_mu_per_POT = rate
 print(f"Final ipa stops rate {ipa_stopped_mu_per_POT}")
 
-
 # get CE normalization:
 def ce_normalization(livetime, rue): # livetime in fractions of year?
   captures_per_stopped_muon = 0.609 # for Al
-  print(POT_per_year , target_stopped_mu_per_POT , captures_per_stopped_muon , livetime , rue, POT_per_year * target_stopped_mu_per_POT * captures_per_stopped_muon * livetime * rue)
+  print("Expected CE's", POT_per_year * target_stopped_mu_per_POT * captures_per_stopped_muon * livetime * rue)
   return POT_per_year * target_stopped_mu_per_POT * captures_per_stopped_muon * livetime * rue
 
+# get IPA Michel normalization:
+def ipaMichel_normalization(livetime):
+  IPA_decays_per_stopped_muon = 0.92 # carbon
+  print("Expected IPA Michel e- ", POT_per_year * ipa_stopped_mu_per_POT * IPA_decays_per_stopped_muon * livetime)
+  return POT_per_year * ipa_stopped_mu_per_POT * IPA_decays_per_stopped_muon * livetime
 
+# get DIO normalization:
+def dio_normalization(livetime, emin):
+  # calculate fraction of spectrum generated
+  #spec = open(os.path.join(os.environ["MUSE_WORK_DIR"],"Production/JobConfig/ensemble_2023/heeck_finer_binning_2016_szafron.tbl")) TODO - needs to be put back once in HEAD
+  spec = open(os.path.join("heeck_finer_binning_2016_szafron.tbl"))
+  energy = []
+  val = []
+  for line in spec:
+    energy.append(float(line.split()[0]))
+    val.append(float(line.split()[1]))
+
+  total_norm = 0
+  cut_norm = 0
+  for i in range(len(val)):
+    total_norm += val[i]
+    if energy[i] >= emin:
+      cut_norm += val[i]
+
+  DIO_per_stopped_muon = 0.391 # 1 - captures_per_stopped_muon
+
+  physics_events = POT_per_year * target_stopped_mu_per_POT * DIO_per_stopped_muon * livetime
+  print("Expected DIO ",physics_events* cut_norm/total_norm)
+  return physics_events * cut_norm/total_norm
+  
 # for testing only
 if __name__ == '__main__':
   ce_normalization(3.5e6/1.1e7, 1e-14)
+  ipaMichel_normalization(3.5e6/1.1e7)
+  dio_normalization(3.5e6/1.1e7,75)
