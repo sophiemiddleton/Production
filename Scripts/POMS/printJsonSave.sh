7899a9526f021c5f0f7c76dc9a63fe690ed54e4f
Iuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2025-05-02 12:42:00 -0500

    Production scripts clean up

--- Scripts/POMS/printJsonSave.sh
+++
@@ -1,7 +0,0 @@
-if [ -f parents.txt ]; then
-    printJson.sh --parents parents.txt ${1} > ${1}.json
-else
-    printJson.sh --no-parents ${1} > ${1}.json
-fi
-sed -i 's/, "md5.*"//' ${1}.json
-cat ${1}.json
